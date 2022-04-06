672801428e2539af90d3f138b8ad61e79dd29f77
paula <paulacsdo2gmail.com>
2022-04-06 17:12:36 +0200

    Funcion modificar sin probar

--- front/biblioteca/src/app/app-routing.module.ts
+++ front/biblioteca/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaAutoresComponent } from './lista-autores/lista-autores.component';
 import { ListaLibrosComponent } from './lista-libros/lista-libros.component';
+import { ModificarLibroComponent } from './modificar-libro/modificar-libro.component';
 
 const routes: Routes = [
   { path: '', component: ListaLibrosComponent },
-  { path: '', component: ListaAutoresComponent, outlet:'secondary'}
+  { path: '', component: ListaAutoresComponent, outlet:'secondary'},
+  { path: 'modificarLibro/:id', component: ModificarLibroComponent}
 ];
 
 @NgModule({
--- front/biblioteca/src/app/modificar-libro/modificar-libro.component.ts
+++ front/biblioteca/src/app/modificar-libro/modificar-libro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Libro } from '../entidades/libro';
+import { ServicioLibrosService } from '../servicio-libros.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-modificar-libro',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modificar-libro.component.css']
 })
 export class ModificarLibroComponent implements OnInit {
+  recogerId="";
+  libroModificado=new Array();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+    public listaModificar: ServicioLibrosService){ 
+      this.libroModificado=[];
+    }
+
+    
 
   ngOnInit(): void {
+    // let id=this.route.snapshot.paramMap.get('id'); 
+    this.route.paramMap.subscribe((params : ParamMap)=> {  
+      this.recogerId=params.get('id')});
+
+    this.listaModificar.modificarLibros(this.recogerId).subscribe(data =>
+      this.libroModificado.push(data));
+  }
+
+  modificarGet() {
+    let verLibro = [];
+
+    for (let lista_anidada of this.libroModificado) {
+      for (let registro of lista_anidada) {
+        verLibro.push(new Libro(...registro));
+      }
+    }
+    return verLibro;
   }
 
 }
